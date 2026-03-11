ecc4d2689f752512141456896721b502ee7d178e
kai-smms <chengkai.hong@smms.com.tw>
2026-03-11 14:39:14 +0800

    修整 README 並增設一些防護

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# ROEquipmentPropertiesChecker
-Check equipmentproperties file different.
+# 功能摘要
+處理 `equipmentproperties` 檔案在更新前後的差異，將其挑選出來並輸出成 `txt` 檔案以利於閱讀。
 
-# 操作前提
-1. 
+# 前置準備
+1. 安裝 `GRF Editor` 或具備類似功能的軟體
+2. 使用上述軟體，將 `equipmentproperties` 檔案以 `txt` 格式輸出
+    - 原本慣用的 lub 轉 lua 工具可能因為太舊了，沒辦法正確轉換 `equipmentproperties` 檔案。
+    - 一時之間我也沒找到新版本的轉換工具，所以此處一律以 `GRF Editor` 等工具手動處理。
+3. 在 `main.py` 所在的目錄中，建立 `Input` 資料夾，並將前一步驟得到的 `equipmentproperties.txt` 放入。
+
+# 操作流程
+1. 執行 `main.py`
+2. 沒了
+
+執行時 `Input/equipmentproperties.txt` 會以當下的電腦時間命名，複製一份相同檔案至 `Temp/` 當中；若 `Temp/` 底下包含 `5` 份以上的檔案，會先進行一次整理，把較舊的檔案移除。
+
+若經過複製後，`Temp/` 至少有 `2` 份檔案時，程式會讀取最新的兩份檔案並進行比對，最終輸出至 `Output` 資料夾中。
+
+# 輸出結果
+## `RawCompareResult.txt`
+比對檔案中，是否有刪除、變更或新增。
+
+若為變更，僅會列出有發現不同的項目，不會告知何處不同。
+
+## `ExplainedCompareResult.txt` (實驗性功能)
+基於 Regex 的判斷，進行局部的功能註釋以利使用者較容易閱讀。
+
+不過內容中仍有很多 Regex 尚未建置或不明原文語法的意義，因此僅是個實驗性功能。
--- main.py
+++ main.py
@@ -20,6 +20,22 @@ INPUT_PATH = EXECUTE_PATH + 'Input\\'
 TEMP_PATH = EXECUTE_PATH + 'Temp\\'
 OUTPUT_PATH = EXECUTE_PATH + 'Output\\'
 
+#region 建置必要目錄
+pbi(indent_number, '# 建置必要目錄')
+indent_number += 1
+if not os.path.exists(INPUT_PATH):
+    os.makedirs(INPUT_PATH)
+    pbi(indent_number, f'- 建立缺少的 Input 資料夾。')
+if not os.path.exists(TEMP_PATH):
+    os.makedirs(TEMP_PATH)
+    pbi(indent_number, f'- 建立缺少的 Temp 資料夾。')
+if not os.path.exists(OUTPUT_PATH):
+    os.makedirs(OUTPUT_PATH)
+    pbi(indent_number, f'- 建立缺少的 Output 資料夾。')
+pbi(indent_number, '- 建置必要目錄 : 完成\n')
+indent_number -= 1
+#endregion
+
 #region 處理新增資料
 pbi(indent_number, '# 處理新增資料')
 indent_number += 1
@@ -49,6 +65,7 @@ indent_number -= 1
 #region 進行資料比對
 pbi(indent_number, '# 進行資料比對')
 indent_number += 1
+existCompareResult = False
 if len(temp_file_list) >= 2:
     pbi(indent_number, f'- 於 Temp 資料夾中具備 {len(temp_file_list)} 個檔案，準備進行資料比對。')
     old_prop_file_data = diff.prop_file_to_dict(TEMP_PATH, temp_file_list[-2])
@@ -57,6 +74,7 @@ if len(temp_file_list) >= 2:
     pbi(indent_number, f'- 資料前處理完成，開始進行資料比對。')
     indent_number += 1
     diff.dict_compare(old_prop_file_data, new_prop_file_data, OUTPUT_PATH, indent_number)
+    existCompareResult = True
 else:
     pbi(indent_number, f'- 於 Temp 資料夾中內的檔案數量不足，無法進行資料比對。')
 pbi(indent_number, '- 進行資料比對 : 完成\n')
@@ -66,7 +84,13 @@ indent_number -= 1
 #region 進行資料註釋
 pbi(indent_number, '# 進行資料註釋')
 indent_number += 1
-effect_explain.effect_explain(OUTPUT_PATH, 'RawCompareResult.txt', 'ExplainedCompareResult.txt')
+if existCompareResult:
+    effect_explain.effect_explain(OUTPUT_PATH, 'RawCompareResult.txt', 'ExplainedCompareResult.txt')
+else:
+    pbi(indent_number, f'- 未執行資料比對，略過資料註釋。')
 pbi(indent_number, '- 進行資料註釋 : 完成\n')
 indent_number -= 1
 #endregion
+
+indent_number = 0
+pbi(indent_number, '# 程式結束執行')
